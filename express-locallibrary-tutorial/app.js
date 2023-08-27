f44d1c725b7562fc11e3cff36ae4609179e7f14a
WuZhengTao <1351434828@qq.com>
2023-08-27 21:02:05 +0800

    stop，create new project to achieve vue+express+mysql

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -6,12 +6,14 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog"); // 导入 catalog 路由
+
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog',catalogRouter);// 将 catalog 路由添加进中间件链
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
